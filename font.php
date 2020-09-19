596fe1b919ace471525c3e966adcb8e33048386d
dr33k <69149388+dr33k@users.noreply.github.com>
2020-09-19 13:53:48 +0100

    Delete font.php

--- font.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html><html>
-<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Cabin" />
-
-<head><style>
-body{
-font-family:Cabin;}
-</style></head>
-</html>
